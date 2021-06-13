e5961bbaa5ff7deb6eda603476cd843083bb15ec
alimmondal <italimbd@gmail.com>
2021-06-13 17:02:08 +0600

    updated

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { FontAwesome, Entypo, AntDesign, Ionicons } from '@expo/vector-icons';
+
+
 import HomeScreen from './src/screens/HomeScreen';
+import NotificationScreen from './src/screens/NotificationScreen';
 import SignInScreen from './src/screens/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 import { AuthContext, AuthProvider } from './src/Providers/AuthProvider';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const HomeStack = createStackNavigator();
+// const HomeStack = createStackNavigator();
 const AuthStack = createStackNavigator();
+const HomeTab = createMaterialBottomTabNavigator();
 
-const HomeStackScreen = () => {
-  return (
-    <HomeStack.Navigator initialRouteName="Home">
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-    </HomeStack.Navigator>
-  );
-};
+// const HomeStackScreen = () => {
+//   return (
+//     <HomeStack.Navigator initialRouteName="Home">
+//       <HomeStack.Screen name="Home" component={HomeScreen} />
+//     </HomeStack.Navigator>
+//   );
+// };
+
+const HomeTabScreen = () => {
+  return(
+    <HomeTab.Navigator initialRouteName="Home">
+    <HomeTab.Screen name="Home" component={HomeScreen} 
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ focused }) => 
+        focused ?(
+          <Entypo name="home" color="white" size={26} />
+        ) : (
+          <AntDesign name="home" color="white" size={22} />
+        ),
+      }}
+    />
+    <HomeTab.Screen name="Notification" component={NotificationScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ focused }) => 
+        focused ?(
+          <Ionicons name="ios-notifications" size={26} color="white" />
+        ) : (
+          <Ionicons name="ios-notifications-outline"size={22} color="white" />
+        ),
+      }}
+    />
+  </HomeTab.Navigator>
+  )
+}
+
 const AuthStackScreen = () => {
   return (
     <AuthStack.Navigator initialRouteName="SignIn">
@@ -27,13 +64,15 @@ const AuthStackScreen = () => {
 
 const App = () => {
   return (
+    <SafeAreaProvider>
     <AuthProvider>
       <AuthContext.Consumer>
         {(auth)=>(<NavigationContainer>
-          {auth.IsLoggedIn ? <HomeStackScreen /> :<AuthStackScreen />}
+          {auth.IsLoggedIn ? <HomeTabScreen /> :<AuthStackScreen />}
         </NavigationContainer>)}
       </AuthContext.Consumer>
     </AuthProvider>
+    </SafeAreaProvider>
   )
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,35 +1,150 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-elements';
-import { AuthContext } from '../Providers/AuthProvider';
-// import { AuthContext } from './src/Providers/AuthProvider';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Card, Button, Text, Avatar, Input, Header } from 'react-native-elements';
 
 
-const HomeScreen = () => {
+import { AntDesign, Entypo } from '@expo/vector-icons';
+import { AuthContext } from '../providers/AuthProvider';
+
+
+
+const HomeScreen = (props) => {
+
+    const post = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse congue lorem sit amet rhoncus vulputate. Mauris eleifend ac libero id molestie. Nam ut augue at dolor convallis vestibulum. Sed at hendrerit nisl, ac hendrerit diam. Mauris vestibulum vehicula vehicula. Aenean pellentesque sem ut ligula convallis tempus"
+
     return (
         <AuthContext.Consumer>
-            {(auth) => (
-                <View>
-                    <Text style={styles.textStyle}>Welcome {auth.CurrentUser.name}</Text>
+            {(auth) => (<ScrollView>
+            <View style={styles.viewStyle}>
+                <Header
+                    leftComponent={{
+                        icon: "menu",
+                        color: "#fff",
+                        onPress: function () {
+                            props.navigation.toggleDrawer();
+                        }
+                    }}
+                    centerComponent={{ text: "The Office", style: { color: "#fff" } }}
+                    rightComponent={{
+                        icon: "lock-outline",
+                        color: "#fff",
+                        onPress: function () {
+                            Auth.setIsLoggedIn(false);
+                            Auth.setCurrentUser({});
+                        }
+                    }}
+                />
+                <Card>
+                    <Input
+                        placeholder="What's on Your mind?"
+                        leftIcon={<Entypo name="pencil" size={24} color="black" />}
+                    />
                     <Button
+                        title="Post"
                         type="outline"
-                        title="Log Out"
                         onPress={function () {
-                            auth.setIsLoggedIn(false);
-                            auth.setCurrentUser({})
+                            props.navigation.toggleDrawer();
                         }}
                     />
-                </View>
+                </Card>
+                <Card>
+                    <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                        <Avatar
+                            containerStyle={{ backgroundColor: "#ffab91" }}
+                            rounded
+                            icon={<AntDesign name="user" size={24} color="white" />}
+                            activeOpacity={1}
+                        />
+                        <Text h4Style={{ padding: 10 }} h4>Jim Halpert</Text>
+                    </View>
+                    <text style={{ fontStyle: "italic" }}>Posted on 10 August, 2020</text>
+                    <Text style={{ paddingVertical: 10, }}>{post}</Text>
+                    <Card.Divider />
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+                        <Button
+                            type="outline"
+                            title="Like(21)"
+                            icon={<AntDesign name="like2" size={24} color="black" />}
+                        />
+                        <Button type="solid" title="Comment(7)" />
+                    </View>
+                </Card>
+                <Card>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Avatar
+                            containerStyle={{ backgroundColor: "#ffab91" }}
+                            rounded
+                            icon={<AntDesign name="user" size={24} color="black" />}
+                            activeOpacity={1}
+                        />
+                        <Text h4Style={{ padding: 10 }} h4>Dwight Schrute</Text>
+                    </View>
+                    <text style={{ fontStyle: "italic" }}>Posted on 22 August, 2020</text>
+                    <Text style={{ paddingVertical: 10, }}>{post}</Text>
+                    <Card.Divider />
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+                        <Button
+                            type="outline"
+                            title="Like(17)"
+                            icon={<AntDesign name="like2" size={24} color="black" />}
+                        />
+                        <Button type="solid" title="Comment(20)" />
+                    </View>
+                </Card>
+            </View>
+            </ScrollView>
             )}
         </AuthContext.Consumer>
     )
-}
+};
 
-export default HomeScreen;
+export default HomeScreen
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30,
         color: 'blue'
+    },
+    viewStyle: {
+        flex: 1,
     }
 })
+
+
+
+
+
+// import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import { Button } from 'react-native-elements';
+// import { AuthContext } from '../Providers/AuthProvider';
+
+
+// const HomeScreen = () => {
+//     return (
+//         <AuthContext.Consumer>
+//             {(auth) => (
+//                 <View>
+//                     <Text style={styles.textStyle}>Welcome {auth.CurrentUser.name}</Text>
+//                     <Button
+//                         type="outline"
+//                         title="Log Out"
+//                         onPress={function () {
+//                             auth.setIsLoggedIn(false);
+//                             auth.setCurrentUser({})
+//                         }}
+//                     />
+//                 </View>
+//             )}
+//         </AuthContext.Consumer>
+//     )
+// }
+
+// export default HomeScreen;
+
+// const styles = StyleSheet.create({
+//     textStyle: {
+//         fontSize: 30,
+//         color: 'blue'
+//     }
+// })
